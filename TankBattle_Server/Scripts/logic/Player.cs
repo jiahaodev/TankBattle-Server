39db9e7282ee0d495c64e793f820f260908f352f
JiahaoWu <jiahaodev@163.com>
2020-02-07 22:50:17 +0800

    添加玩家、玩家数据类

---
+++ TankBattle_Server/Scripts/logic/Player.cs
@@ -0,0 +1,33 @@
+﻿/****************************************************
+	文件：Player.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/07 22:48   	
+	功能：玩家类
+*****************************************************/
+using System;
+
+public class Player
+{
+    public string id = "";
+
+    public ClientState state; 
+
+    public Player(ClientState state) {
+        this.state = state;
+    }
+
+    //临时数据，如：坐标
+    public int x;
+    public int y;
+    public int z;
+    //数据库数据
+    public PlayerData data;
+
+    //发送消息
+    public void Send(MsgBase msgBase) {
+        NetManager.Send(state,msgBase);
+    }
+
+}
+
---
+++ TankBattle_Server/Scripts/logic/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿/****************************************************
+	文件：PlayerData.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/07 22:48   	
+	功能：玩家数据类
+*****************************************************/
+
+public class PlayerData
+{
+    //金币
+    public int coin = 0;
+    //记事本
+    public string text = "new text";
+}
+
