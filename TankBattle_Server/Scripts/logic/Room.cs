726e7e1bdcbfc535c2733a6de3436abad10245f1
JiahaoWu <jiahaodev@163.com>
2020-02-08 01:38:10 +0800

    添加定义：游戏房间

---
+++ TankBattle_Server/Scripts/logic/Room.cs
@@ -0,0 +1,192 @@
+﻿/****************************************************
+	文件：Room.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/08 1:22   	
+	功能：游戏房间
+*****************************************************/
+using System;
+using System.Collections.Generic;
+
+public class Room
+{
+    //房间id
+    public int id = 0;
+    //最大玩家数
+    public int maxPlayer = 6;
+    //玩家列表
+    public Dictionary<string, bool> playerIds = new Dictionary<string, bool>();
+    //房主id
+    public string ownerId = "";
+
+    //房间状态
+    public enum Status {
+        PREPARE = 0,
+        FIGHT = 1,
+    }
+
+    public Status status = Status.PREPARE;
+
+
+    //添加玩家
+    public bool AddPlayer(string id)
+    {
+        //获取玩家
+        Player player = PlayerManager.GetPlayer(id);
+        if (player == null)
+        {
+            Console.WriteLine("room.AddPlayer fail, player is null");
+            return false;
+        }
+        //房间人数
+        if (playerIds.Count >= maxPlayer)
+        {
+            Console.WriteLine("room.AddPlayer fail, reach maxPlayer");
+            return false;
+        }
+        //准备状态才能加人
+        if (status != Status.PREPARE)
+        {
+            Console.WriteLine("room.AddPlayer fail, not PREPARE");
+            return false;
+        }
+        //已经在房间里
+        if (playerIds.ContainsKey(id))
+        {
+            Console.WriteLine("room.AddPlayer fail, already in this room");
+            return false;
+        }
+        //加入列表
+        playerIds[id] = true;
+        //设置玩家数据
+        player.camp = SwitchCamp();
+        player.roomId = this.id;
+        //设置房主
+        if (ownerId == "")
+        {
+            ownerId = player.id;
+        }
+        //广播
+        Broadcast(ToMsg());
+        return true;
+    }
+
+    //删除玩家
+    public bool RemovePlayer(string id)
+    {
+        //获取玩家
+        Player player = PlayerManager.GetPlayer(id);
+        if (player == null)
+        {
+            Console.WriteLine("room.RemovePlayer fail, player is null");
+            return false;
+        }
+        //没有在房间里
+        if (!playerIds.ContainsKey(id))
+        {
+            Console.WriteLine("room.RemovePlayer fail, not in this room");
+            return false;
+        }
+        //删除列表
+        playerIds.Remove(id);
+        //设置玩家数据
+        player.camp = 0;
+        player.roomId = -1;
+        //设置房主
+        if (ownerId == player.id)
+        {
+            ownerId = SwitchOwner();
+        }
+        //房间为空
+        if (playerIds.Count == 0)
+        {
+            RoomManager.RemoveRoom(this.id);
+        }
+        //广播
+        Broadcast(ToMsg());
+        return true;
+    }
+
+    //分配阵营
+    public int SwitchCamp()
+    {
+        //计数
+        int count1 = 0;
+        int count2 = 0;
+        foreach (string id in playerIds.Keys)
+        {
+            Player player = PlayerManager.GetPlayer(id);
+            if (player.camp == 1) { count1++; }
+            if (player.camp == 2) { count2++; }
+        }
+        //选择
+        if (count1 <= count2)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
+
+    //是不是房主
+    public bool isOwner(Player player)
+    {
+        return player.id == ownerId;
+    }
+
+    //选择房主
+    public string SwitchOwner()
+    {
+        //选择第一个玩家
+        foreach (string id in playerIds.Keys)
+        {
+            return id;
+        }
+        //房间没人
+        return "";
+    }
+
+
+    //广播消息
+    public void Broadcast(MsgBase msg)
+    {
+        foreach (string id in playerIds.Keys)
+        {
+            Player player = PlayerManager.GetPlayer(id);
+            player.Send(msg);
+        }
+    }
+
+    //生成MsgGetRoomInfo协议
+    public MsgBase ToMsg()
+    {
+        MsgGetRoomInfo msg = new MsgGetRoomInfo();
+        int count = playerIds.Count;
+        msg.players = new PlayerInfo[count];
+        //players
+        int i = 0;
+        foreach (string id in playerIds.Keys)
+        {
+            Player player = PlayerManager.GetPlayer(id);
+            PlayerInfo playerInfo = new PlayerInfo();
+            //赋值
+            playerInfo.id = player.id;
+            playerInfo.camp = player.camp;
+            playerInfo.win = player.data.win;
+            playerInfo.lost = player.data.lost;
+            playerInfo.isOwner = 0;
+            if (isOwner(player))
+            {
+                playerInfo.isOwner = 1;
+            }
+
+            msg.players[i] = playerInfo;
+            i++;
+        }
+        return msg;
+    }
+
+}
+
