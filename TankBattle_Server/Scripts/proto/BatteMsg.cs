b04ea4a8d9cfbd7ffcfe56e6f4266cb3fcf2d0c1
JiahaoWu <jiahaodev@163.com>
2020-02-08 02:15:40 +0800

    完善战斗消息定义

--- TankBattle_Server/Scripts/proto/BatteMsg.cs
+++ TankBattle_Server/Scripts/proto/BatteMsg.cs
@@ -6,20 +6,44 @@
 	功能：战斗相关消息
 *****************************************************/
 
-public class MsgMove : MsgBase
+//坦克信息
+[System.Serializable]
+public class TankInfo
 {
-    public MsgMove() { protoName = "MsgMove"; }
-    //坐标
-    public int x = 0;
-    public int y = 0;
-    public int z = 0;
+    public string id = "";  //玩家id
+    public int camp = 0;    //阵营
+    public int hp = 0;      //生命值
+
+    public float x = 0;     //位置
+    public float y = 0;
+    public float z = 0;
+    public float ex = 0;    //旋转
+    public float ey = 0;
+    public float ez = 0;
 }
 
 
-public class MsgAttack : MsgBase
+//进入战场（服务端推送）
+public class MsgEnterBattle : MsgBase
 {
-    public MsgAttack() { protoName = "MsgAttack"; }
-
-    public string desc = "127.0.0.1:6543";
+    public MsgEnterBattle() { protoName = "MsgEnterBattle"; }
+    //服务端回
+    public TankInfo[] tanks;
+    public int mapId = 1;	//地图，只有一张
 }
 
+//战斗结果（服务端推送）
+public class MsgBattleResult : MsgBase
+{
+    public MsgBattleResult() { protoName = "MsgBattleResult"; }
+    //服务端回
+    public int winCamp = 0;	 //获胜的阵营
+}
+
+//玩家退出（服务端推送）
+public class MsgLeaveBattle : MsgBase
+{
+    public MsgLeaveBattle() { protoName = "MsgLeaveBattle"; }
+    //服务端回
+    public string id = "";	//玩家id
+}
