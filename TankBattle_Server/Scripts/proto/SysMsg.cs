becbab414ef97c16f6e3d390096bb806fd6d12b5
JiahaoWu <jiahaodev@163.com>
2020-02-08 00:10:13 +0800

    添加系统消息定义

---
+++ TankBattle_Server/Scripts/proto/SysMsg.cs
@@ -0,0 +1,25 @@
+﻿/****************************************************
+	文件：SysMsg.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/07 23:57   	
+	功能：系统消息
+*****************************************************/
+
+
+public class MsgPing : MsgBase
+{
+    public MsgPing()
+    {
+        protoName = "MsgPing";
+    }
+}
+
+
+public class MsgPong : MsgBase
+{
+    public MsgPong()
+    {
+        protoName = "MsgPong";
+    }
+}
