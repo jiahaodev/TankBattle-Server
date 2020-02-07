70d63317c1e98f27e399ba5f10de582c32eb6bb2
JiahaoWu <jiahaodev@163.com>
2020-02-07 22:40:48 +0800

    定义客户端连接状态

---
+++ TankBattle_Server/Scripts/net/ClientState.cs
@@ -0,0 +1,18 @@
+﻿/****************************************************
+	文件：ClientState.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/07 22:09   	
+	功能：客户端连接状态
+*****************************************************/
+using System.Net.Sockets;
+
+
+public class ClientState
+{
+    public Socket socket;
+    public ByteArray readBuffer = new ByteArray();
+    public long lastPingTime = 0;
+    //public Player player;
+}
+
