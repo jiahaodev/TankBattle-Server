2cf84a466c7c36a0430af67b45904f629c6d9fe5
JiahaoWu <jiahaodev@163.com>
2020-02-08 00:48:12 +0800

    添加战斗协议处理

---
+++ TankBattle_Server/Scripts/logic/BattleMsgHandler.cs
@@ -0,0 +1,23 @@
+﻿/****************************************************
+	文件：BattleMsgHandler.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/08 0:45   	
+	功能：战斗相关消息处理
+*****************************************************/
+
+using System;
+
+
+public partial class MsgHandler
+{
+    public static void MsgMove(ClientState c, MsgBase msgBase)
+    {
+        MsgMove msgMove = (MsgMove)msgBase;
+        Console.WriteLine(msgMove.x);
+        msgMove.x++;
+        NetManager.Send(c, msgMove);
+    }
+}
+
+
