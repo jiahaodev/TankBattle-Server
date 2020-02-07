e4b98809471ee006a58c50af17f9505b86c29f89
JiahaoWu <jiahaodev@163.com>
2020-02-08 00:10:33 +0800

    添加消息处理者

---
+++ TankBattle_Server/Scripts/logic/SysMsgHandler.cs
@@ -0,0 +1,18 @@
+﻿/****************************************************
+	文件：SysMsgHandler.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/07 23:53   	
+	功能：消息处理者
+*****************************************************/
+
+public partial class MsgHandler
+{
+
+    public static void MsgPing(ClientState state,MsgBase magBase) {
+        state.lastPingTime = NetManager.GetTimeStamp();
+        MsgPong msgPong = new MsgPong();
+        NetManager.Send(state,msgPong);
+    }
+}
+
