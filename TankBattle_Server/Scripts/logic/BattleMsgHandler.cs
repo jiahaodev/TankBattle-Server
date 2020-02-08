77b4d7ad301dbfba7c634484a3f75434a95e71a1
JiahaoWu <jiahaodev@163.com>
2020-02-08 14:44:33 +0800

    注释掉旧的战斗测试

--- TankBattle_Server/Scripts/logic/BattleMsgHandler.cs
+++ TankBattle_Server/Scripts/logic/BattleMsgHandler.cs
@@ -11,13 +11,13 @@ using System;
 
 public partial class MsgHandler
 {
-    public static void MsgMove(ClientState c, MsgBase msgBase)
-    {
-        MsgMove msgMove = (MsgMove)msgBase;
-        Console.WriteLine(msgMove.x);
-        msgMove.x++;
-        NetManager.Send(c, msgMove);
-    }
+    //public static void MsgMove(ClientState c, MsgBase msgBase)
+    //{
+    //    MsgMove msgMove = (MsgMove)msgBase;
+    //    Console.WriteLine(msgMove.x);
+    //    msgMove.x++;
+    //    NetManager.Send(c, msgMove);
+    //}
 }
 
 
